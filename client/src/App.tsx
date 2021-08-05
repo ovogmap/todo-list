be1dc1c4b03a80afc652beac04f464bc85586ff2
ovogmap <djtheend@naver.com>
2021-08-05 23:53:04 +0900

    docs: 구조 잡는걸 생각하느라 2시간 앉아 있었는데.. 더생각 해봐야 할 듯합당...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,7 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  useEffect(() => {
-    axios.get('http://localhost:5000/').then((v) => console.log(v))
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div>Todo app</div>
 }
 
 export default App
